8f5d3b40a2f1a06023dc9283198a8aa78b9678df
Felipe Schossler <felipealvesschossler@gmail.com>
2018-12-04 22:24:08 -0200

    First commit with chapter 1 already done(not the same way of course)

---
+++ ByteBank/ByteBank/Program.cs
@@ -0,0 +1,19 @@
+﻿using ByteBank.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Diretor felipe = new Diretor();
+
+        }
+    }
+}
